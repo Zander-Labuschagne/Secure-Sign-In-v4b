0716bbcf4af3a20486a892e2b06805756fd278d0
Zander Labuschagne <zander.labuschagne@protonmail.ch>
2019-08-22 14:33:36 +0200

    updated algorithm to match the mobile version which is the latest

--- SecureSignIn/secure_sign_in.cpp
+++ SecureSignIn/secure_sign_in.cpp
@@ -1,132 +1,140 @@
-//Exception handling?
-//Moet ek soos die pointers gebruik? Of arrays? of vectors?
-//Benchmark die verskillende implementyerings
-
+/**
+ * Copyright (C) 2019 Zander Labuschagne. All rights reserved.
+ * @version 4.0.0 22/08/19
+ * @since 4.0.0
+ *
+ * Authors:
+ *         @author Zander Labuschagne <zander.labuschagne@protonmail.ch>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU GPL 3.0 License, as published by the
+ * Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GPL 3.0 License for more details.
+ *
+ * You should have received a copy of the GNU GPL 3.0 License
+ * along with this program; if not, write to the author,
+ * contact details above in line 7.
+ */
 #include "secure_sign_in.hpp"
 
-#include <iostream>
+#include <stdlib.h>
 
-/*
- * Class containing encryption function(s) for the Secure Sign In application
- *
- * Author:
- *	Zander Labuschagne <zander.labuschagne@protonmail.ch>
- *
- * I am still learning C++ so if anything is unacceptable or a violation to some standards please inform me.
- * TODO: Experiment with vectors
+/**
+ * @brief Default constructor
+ * secure_sign_in.cpp
+ * @since 4.0.0
 */
-
 SecureSignIn::SecureSignIn()
 {
 
 }
 
-/*
-* Function to encrypt text, used for the encryption of passwords in the Secure Sign In application.
-* user_password contains plain text password to be encrypted.
+/**
+ * @brief helper function to determine the actual length of the character array.
+ * secure_sign_in.cpp
+ * calls encrypt function.
+ * @since 4.0.0
+ */
+unsigned short SecureSignIn::get_length(const char* ptr_array)
+{
+	unsigned short ix;
+	for (ix = 0; *(ptr_array + ix) != '\0'; ++ix);
+
+	return ++ix;  // Include the last null character to signal end, otherwise weird things happen
+}
+
+/**
+* @brief Function to encrypt text, used for the encryption of passwords in the Secure Sign In applications.
+* password_in contains plain text password to be encrypted.
 * key contains the key entered by the user, could be anything, even the password.
 * limit specifies the limit of the password length, some websites have limitations on the password length.
-* returns an encrypted password that should be more complex and appears random.
+* @returns an encrypted password that should be more complex and appears random.
+* secure_sign_in.cpp
+* @since 4.0.0
 */
-char* SecureSignIn::encrypt(const char *user_password, const char *key, unsigned short limit)
+char* SecureSignIn::encrypt(const char *password_in, const char *key, unsigned short limit)
 {
-	char *system_password;
-	system_password = (char*)malloc(256);
+	char *password = (char*)malloc(256);
+	for (int z = 0; z < 256; ++z)
+		*(password + z) = '\0';
 	unsigned short key_index = 0;
 	unsigned short i = 0;
-	char *spec_chars;
-	spec_chars = (char*)malloc(256);
+	char *spec_chars = (char*)malloc(256);
+	for (int z = 0; z < 256; ++z)
+		*(spec_chars + z) = '\0';
 	unsigned short ii = 0;
-	unsigned short pos = 0;
 	unsigned short spec_char_count = 0;
-
-
-	//for(char t : user_password)
 	unsigned short password_length = 0;
-	for (char t = *user_password; t != '\0'; t = *(user_password + ++password_length)) {
-		if (t >= 65 && t <= 90) { //Encrypting uppercase characters
+
+	for (char t = *password_in; t != '\0'; t = *(password_in + ++password_length)) {
+		if (t >= 65 && t <= 90) { // Encrypting uppercase characters
 			short temp = t - 65 + (*(key + key_index) - 65);
 			if (temp < 0)
 				temp += 26;
 			if (temp <= 0)
 				temp += 26;
 
-			*(system_password + i++) = (char)(65 + (temp % 26));
-			//if(++key_index == key.length)
+			*(password + i++) = (char)(65 + (temp % 26));
+
 			if (*(key + ++key_index) == '\0')
 				key_index = 0;
-		} else if (t >= 97 && t <= 122) { //Encrypting lowercase characters
+		} else if (t >= 97 && t <= 122) { // Encrypting lowercase characters
 			short temp = t - 97 + (*(key + key_index) - 97);
-			if(temp < 0)
+			if (temp < 0)
 				temp += 26;
-			if(temp < 0)
+			if (temp < 0)
 				temp += 26;
 
-			*(system_password + i++) = (char)(97 + (temp % 26));
-			//if(++key_index == key.length)
-			if(*(key + ++key_index) == '\0')
+			*(password + i++) = (char)(97 + (temp % 26));
+
+			if (*(key + ++key_index) == '\0')
 				key_index = 0;
-		} else { //Encrypting special characters
-
-			*(spec_chars + ii++) = (char)(pos + 65);
+		} else { // Encrypting special characters and positions of these special characters respectively
+			*(spec_chars + ii++) = (char)(password_length + 65);
 			*(spec_chars + ii++) = t;
-			spec_char_count++;
 		}
-		pos++;
 	}
 
-	char *final_password;
-	final_password = (char*)malloc(256);
+	char *password_proc = (char*)malloc(256);
 	i = 0;
-	*(final_password + i++) = (char)(spec_char_count == 0 ? 65 : (--spec_char_count + 65)); //Encrypting the number of special characters
-	for (char t = *spec_chars; t != '\0'; i++, t = *(spec_chars + i - 1)) //Encrypting special characters and positions of special characters
-		*(final_password + i) = t;
-	ii = i;
-	for (char t = *system_password; t != '\0'; i++, t = *(system_password + i - ii)) //Adding the encrypted password to the encrypted special characters and positions
-		*(final_password + i) = t;
 
-	free(system_password);
+	*(password_proc + i++) = (char)(spec_char_count == 0 ? 65 : (--spec_char_count + 65)); // Encrypting the number of special characters
+
+	for (char t = *spec_chars; t != '\0'; i++, t = *(spec_chars + i - 1)) //Merging special characters and positions of special characters into cipher password
+		*(password_proc + i) = t;
+	ii = i;
+
+	for (char t = *password; t != '\0'; i++, t = *(password + i - ii)) // Adding the encrypted password to the encrypted special characters and positions in the cipher password
+		*(password_proc + i) = t;
+
+	free(password);
 	free(spec_chars);
 
-	short ext = -1;
-	if (i > 32 && limit > 12)
-		while (1) {
-			std::cout << "Warning: Password is longer than 32 characters" << std::endl << "Would you like to shorten the password to the 32 limit? (y/n)" << std::endl;
-			std::string input;
-			std::cin >> input;
-			if (toupper(input[0]) == 'Y') {
-				ext = 1;
-				break;
-			} else if (toupper(input[0]) == 'N') {
-				break;
-			}
-			std::cout << "You have entered an inapropriate answer given the issue. Please try again.";
-		}
+	unsigned short length = this->get_length(password_proc); // Count the number of characters to eliminate a bunch of null characters at the end
 
-	unsigned short length;
-	for (length = 0; *(final_password + length) != '\0'; ++length);//Die maak die password so klein as moontlik(geen null characters aan die einde) -> kan ek die bypass omdat dit C++ pointers is?
-	char* cipher_password;
-	cipher_password = (char*)malloc(length);
-	for (unsigned short xi = 0; xi < length /*&& xi < cipher_password.length*/ ; ++xi)
-		*(cipher_password + xi) = *(final_password + xi);
+	char *password_out = (char*)malloc(length);
+	for (unsigned short xi = 0; xi < length; ++xi) // Move password from big memory to just the right amount of memory
+		*(password_out + xi) = *(password_proc + xi);
 
-	free(final_password);
+	free(password_proc);
 
-	//Suffle password for extra security
-	char *evens; //Kan dalk vectors ook gebruik
-	evens = (char*)malloc(256);
-	char *odds;
-	odds = (char*)malloc(256);
+	char *evens = (char*)malloc(256);//TODO: Pasop!
+	char *odds = (char*)malloc(256);
 	unsigned short xii = 0, xiii = 0;
-	for (unsigned short iii = 0; iii < length; ++iii)
-		if ((unsigned short)(*(cipher_password + iii) % 2 == 0))
-			*(evens + xii++) = *(cipher_password + iii);
+	for (unsigned short iii = 0; iii < length - 1; ++iii) // - 1 to exclude the last null character
+		if ((unsigned short)(*(password_out + iii) % 2 == 0))
+			*(evens + xii++) = *(password_out + iii);
 		else
-			*(odds + xiii++) = *(cipher_password + iii);
-	char *odds_reversed;
-	odds_reversed = (char*)malloc(xiii);
-	for (unsigned short xvi = 0, xvii = xiii; xvii > 0; xvii--, ++xvi) //Reverse odds, like stack
+			*(odds + xiii++) = *(password_out + iii);
+
+	char *odds_reversed = (char*)malloc(xiii);
+	for (unsigned short xvi = 0, xvii = xiii; xvii > 0; --xvii, ++xvi) // Reverse odds, like stack
 		*(odds_reversed + xvi) = *(odds + xvii - 1);
+
 	free(odds);
 
 	unsigned short iv = 0;
@@ -134,52 +142,55 @@ char* SecureSignIn::encrypt(const char *user_password, const char *key, unsigned
 	unsigned short xv = 0;
 	while (xii > 0 || xiii > 0) { //Obfuscation
 		if (xiii > 0) {
-			*(cipher_password + iv++) = *(odds_reversed + xiv++);
-			xiii--;
+			*(password_out + iv++) = *(odds_reversed + xiv++);
+			--xiii;
 		}
 		if (xii > 0) {
-			*(cipher_password + iv++) = *(evens + xv++);
-			xii--;
+			*(password_out + iv++) = *(evens + xv++);
+			--xii;
 		}
 	}
 
-	free(odds_reversed);
 	free(evens);
+	free(odds_reversed);
 
-	//Encrypt special chars further
-	for (unsigned short v = 0; v < length/*cipher_password.lerngth*/; ++v)
-		if ((unsigned short)(*(cipher_password + v)) <= 47) {
-			*(cipher_password + v) += 10;
-		} else if ((unsigned short)(*(cipher_password + v)) > 47 && (unsigned short)(*(cipher_password + v)) < 64) {
-			*(cipher_password + v) -= 5;
-		} else if ((unsigned short)(*(cipher_password + v)) > 90 && (unsigned short)(*(cipher_password + v)) <= 96) {
-			if(length % 2 == 0)
-				*(cipher_password + v) += 2;
+	for (unsigned short v = 0; v < length - 1; ++v) // Encrypt special characters further. Remember not to encrypt the last null character(- 1)
+		if ((unsigned short)(*(password_out + v)) <= 47)
+			*(password_out + v) += 10;
+		else if ((unsigned short)(*(password_out + v)) > 47 && (unsigned short)(*(password_out + v)) < 64)
+			*(password_out + v) -= 5;
+		else if ((unsigned short)(*(password_out + v)) > 90 && (unsigned short)(*(password_out)) <= 96) {
+			if (length % 2 == 0)
+				*(password_out + v) += 2;
 			else
-				*(cipher_password + v) -= 2;
+				*(password_out + v) -= 2;
 		}
 
-	//Replacing unloved characters
-	for (unsigned short vi = 0; vi < length; ++vi) //As ek length so gaan gebruik moet hy const word
-		if ((unsigned short)(*(cipher_password + vi)) == 34)
-			*(cipher_password + vi) = 123;
-		else if ((unsigned short)(*(cipher_password + vi)) == 38)
-			*(cipher_password + vi) = 124;
-		else if ((unsigned short)(*(cipher_password + vi)) == 60)
-			*(cipher_password + vi) = 125;
-		else if ((unsigned short)(*(cipher_password + vi)) == 62)
-			*(cipher_password + vi) = 126;
+	for (unsigned short vi = 0; vi < length; ++vi) //Replacing unloved characters
+		if ((unsigned short)(*(password_out + vi)) == 34)
+			*(password_out + vi) = 123;
+		else if((unsigned short)(*(password_out + vi)) == 38)
+			*(password_out + vi) = 124;
+		else if ((unsigned short)(*(password_out + vi)) == 60)
+			*(password_out + vi) = 125;
+		else if ((unsigned short)(*(password_out + vi)) == 62)
+			*(password_out + vi) = 126;
 
-	//Limitations
-	if(ext == 1 || limit < 32) {
-		char* cipher_password_limited = (char*)malloc(length);
-		for(unsigned short vii = 0; vii < length && vii < limit; ++vii)
-			*(cipher_password_limited + vii) = *(cipher_password + vii);
+	if (limit < 32) { // Password length limitations
+		char *password_limited = (char*)malloc(length);
+		for (unsigned short vii = 0; vii < length && vii < limit; ++vii)
+			*(password_limited + vii) = *(password_out + vii);
+		unsigned short length_limited = get_length(password_limited); // Count the number of characters to eliminate a bunch of null characters at the end
 
-		free(cipher_password);
+		char *password_out_limited = (char*)malloc(length_limited);
+		for (unsigned short xi = 0; xi < length_limited; ++xi) // Move password from big memory to just the right amount of memory
+			*(password_out_limited + xi) = *(password_limited + xi);
 
-		return cipher_password_limited; //TODO:Check
+		free(password_limited);
+		free(password_out);
+
+		return password_out_limited;
 	}
 
-	return cipher_password;
+	return password_out;
 }
--- SecureSignIn/secure_sign_in.hpp
+++ SecureSignIn/secure_sign_in.hpp
@@ -1,20 +1,40 @@
+/**
+ * Copyright (C) 2019 Zander Labuschagne. All rights reserved.
+ * @version 4.0.0 22/08/19
+ * @since 4.0.0
+ *
+ * Authors:
+ *         @author Zander Labuschagne <zander.labuschagne@protonmail.ch>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU GPL 3.0 License, as published by the
+ * Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GPL 3.0 License for more details.
+ *
+ * You should have received a copy of the GNU GPL 3.0 License
+ * along with this program; if not, write to the author,
+ * contact details above in line 7.
+ */
+
 #ifndef SECURE_SIGN_IN_HPP
 #define SECURE_SIGN_IN_HPP
 
-/*
- * Class containing encryption function(s) for the Secure Sign In application
- *
- * Author:
- *	Zander Labuschagne <zander.labuschagne@protonmail.ch>
- *
- * I am still learning C++ so if anything is unacceptable or a violation to some standards please inform me.
+/**
+ * @brief Class containing encryption function(s) for the Secure Sign In applications
+ * @since 4.0.0
+ * secure_sign_in.hpp
 */
-
 class SecureSignIn
 {
 public:
 	SecureSignIn();
-	char *encrypt(const char *, const char *, unsigned short);
+	char *encrypt(const char *, const char *, const unsigned short);
+private:
+	unsigned short get_length(const char *);
 };
 
 #endif // SECURE_SIGN_IN_HPP
